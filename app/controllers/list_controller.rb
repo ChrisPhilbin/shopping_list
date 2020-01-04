2fad6b01a7275fc19924c50a8ae2ba80a84cba1a
Chris Philbin <chris@chrisphilbin.net>
2020-01-04 09:14:11 -0500

    Stubbing out 7 index actions within list_controller

---
+++ app/controllers/list_controller.rb
@@ -0,0 +1,31 @@
+class ListController < ApplicationController
+
+	def new
+
+	end
+
+	def index
+
+	end
+
+	def create
+
+	end
+
+	def show
+
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+
+end
