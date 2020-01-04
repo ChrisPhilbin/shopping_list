7278ba47d99f3834ea81e356a628b14450563eca
Chris Philbin <chris@chrisphilbin.net>
2020-01-04 09:22:01 -0500

    Updating create action within list_controller and also implementing strong params

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,15 +1,23 @@
 class ListController < ApplicationController
 
 	def new
+		@list = List.new
 
 	end
 
 	def index
-
+		@lists = List.all
 	end
 
 	def create
-
+		@list = List.save(list_params)
+		if @list.save
+			flash[:notice] = "Your list was successfully created!"
+			redirect_to @list_path(@list.id)
+		else
+			flash.now.alert = "Something went wrong!  Please try again!"
+			render :new
+		end
 	end
 
 	def show
@@ -26,6 +34,12 @@ class ListController < ApplicationController
 
 	def destroy
 
+	end
+
+	private
+
+	def list_params
+		params.require(:list).permit(:name)
 	end
 
 end
