7893a364e7b67c1eca586e313291766649481243
Chris Philbin <chris@chrisphilbin.net>
2020-01-07 20:30:56 -0500

    Adding react-router to dependancies and starting to implmenet basic react todo front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      str: '',
+      items: []
+    }
+  }
+
+  onChange = (event) => {
+    this.setState({str: event.target.value})
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      str: '',
+      items: [...this.state.items, this.state.str]
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2019_12_30_180417) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
-    t.integer "list_id"
+    t.bigint "list_id"
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -28,4 +31,5 @@ ActiveRecord::Schema.define(version: 2019_12_30_180417) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "lists"
 end
