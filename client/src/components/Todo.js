f2b61d2e1dad61085614d87d38749bd63fc7accb
Chris Philbin <chris@chrisphilbin.net>
2020-01-09 13:48:21 -0500

    Creating presentational component for individual todo LI items

---
+++ client/src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from './prop-types'
+
+const todo = ({onclick, completed, text}) => (
+	<li
+	onclick={onclick}
+	style={{textDecoration: completed? 'line-through' : 'none'}}
+	>
+	{text}
+	</li>
+)
+
+Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired
+}
+export default Todo
